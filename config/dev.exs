461d069d1e51467bbd6d284f4878cd56a831a45c
niku <niku@niku.name>
2015-08-17 02:59:21 +0900

    First import
    $ mix phoenix.new phoenix_chat_sandbox --app chat --no-ecto

---
+++ config/dev.exs
@@ -0,0 +1,27 @@
+use Mix.Config
+
+# For development, we disable any cache and enable
+# debugging and code reloading.
+#
+# The watchers configuration can be used to run external
+# watchers to your application. For example, we use it
+# with brunch.io to recompile .js and .css sources.
+config :chat, Chat.Endpoint,
+  http: [port: 4000],
+  debug_errors: true,
+  code_reloader: true,
+  cache_static_lookup: false,
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin"]]
+
+# Watch static and templates for browser reloading.
+config :chat, Chat.Endpoint,
+  live_reload: [
+    patterns: [
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif)$},
+      ~r{web/views/.*(ex)$},
+      ~r{web/templates/.*(eex)$}
+    ]
+  ]
+
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "[$level] $message\n"
